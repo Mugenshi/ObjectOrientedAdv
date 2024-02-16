8c248c287581fdf50ef972e6edfe01f5236750e6
Copter <145657977+Mugenshi@users.noreply.github.com>
2024-02-16 16:47:14 +0700

    added true first person
    delete first person files, modified a third person template to a first person

---
+++ Source/ObjectOrientedAdv/LightSwitchBoth.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ObjectOrientedAdv.h"
+#include "LightSwitchBoth.h"
+
+ALightSwitchBoth::ALightSwitchBoth()
+    {
+
+        DesiredIntensity = 3000.0f;
+
+        PointLight1 = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight1"));
+        PointLight1->Intensity = DesiredIntensity;
+
+        Sphere1 = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere1"));
+        Sphere1->InitSphereRadius(250.0f);
+        Sphere1->SetupAttachment(RootComponent);
+
+        Sphere1->OnComponentBeginOverlap.AddDynamic(this, &ALightSwitchBoth::OnOverlapBegin);       // set up a notification for when this component overlaps something
+        Sphere1->OnComponentEndOverlap.AddDynamic(this, &ALightSwitchBoth::OnOverlapEnd);       // set up a notification for when this component overlaps something
+    }
+
+void ALightSwitchBoth::OnOverlapBegin_Implementation(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		ToggleLight();
+	}
+}
+
+void ALightSwitchBoth::OnOverlapEnd_Implementation(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		ToggleLight();
+	}
+}
+
+void ALightSwitchBoth::ToggleLight()
+{
+	PointLight1->ToggleVisibility();
+}
+
