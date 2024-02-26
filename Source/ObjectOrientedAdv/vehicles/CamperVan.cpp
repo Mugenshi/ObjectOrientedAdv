63a6c4e4f9efab0136b3ca2775c8c1222d572d94
Biggypath <patarapongtha@gmail.com>
2024-02-26 21:05:46 +0700

    ..
    .

---
+++ Source/ObjectOrientedAdv/vehicles/CamperVan.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CamperVan.h"
+#include "ObjectOrientedAdv/ObjectOrientedAdv.h"
+
+// Sets default values
+ACamperVan::ACamperVan()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Speed = 60.0f;
+	Weight = 2000.0f;
+
+}
+
+// Called when the game starts or when spawned
+void ACamperVan::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACamperVan::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+float ACamperVan::GetSpeed_Implementation()
+{
+	return Speed;
+}
+
+float ACamperVan::GetWeight_Implementation()
+{
+	return Weight;
+}
+
