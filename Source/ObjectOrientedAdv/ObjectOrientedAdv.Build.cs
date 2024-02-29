a9185cd80d788cbb0d7604fc614cd3a5901afff8
Copter <145657977+Mugenshi@users.noreply.github.com>
2024-02-29 17:12:21 +0700

    commit saved my life
    added another level

--- Source/ObjectOrientedAdv/ObjectOrientedAdv.Build.cs
+++ Source/ObjectOrientedAdv/ObjectOrientedAdv.Build.cs
@@ -8,7 +8,7 @@ public class ObjectOrientedAdv : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "NavigationSystem", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/ObjectOrientedAdv/home/Book.h
+++ Source/ObjectOrientedAdv/home/Book.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Book.generated.h"
 
-UCLASS(MinimalAPI, Blueprintable)
+UCLASS(Blueprintable)
 class OBJECTORIENTEDADV_API ABook : public AActor
 {
 	GENERATED_BODY()
